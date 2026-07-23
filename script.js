4da7ca10e5b8ab98b451d2b3b3e5f5e9248428e6
Savannah-K <savannahk156@gmail.com>
2026-07-23 19:18:42 -0400

    Scrapping CSS MP3 Player for images

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let poster = document.querySelector('#poster');
 let singer = document.querySelector('#singer');
 let date = document.querySelector('#date');
 
-let back = document.querySelector(".fa-fast-backward");
-let play = document.querySelector(".fa-play");
-let pause = document.querySelector(".fa-pause");
-let next = document.querySelector(".fa-fast-forward"); 
+let back = document.querySelector('.fa-fast-backward');
+let play = document.querySelector('.fa-play');
+let pause = document.querySelector('.fa-pause');
+let next = document.querySelector('.fa-fast-forward');
