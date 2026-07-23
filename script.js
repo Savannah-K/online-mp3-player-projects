06976f15243267fbe945beffc0af565220365d84
Savannah-K <savannahk156@gmail.com>
2026-07-23 18:59:01 -0400

    trying to set up mp3 player interface and functions

---
+++ script.js
@@ -0,0 +1,9 @@
+let music = document.querySelector('#mymusic');
+let poster = document.querySelector('#poster');
+let singer = document.querySelector('#singer');
+let date = document.querySelector('#date');
+
+let back = document.querySelector(".fa-fast-backward");
+let play = document.querySelector(".fa-play");
+let pause = document.querySelector(".fa-pause");
+let next = document.querySelector(".fa-fast-forward"); 
